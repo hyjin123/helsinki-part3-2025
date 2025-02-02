fb04efaa20cd92e5569bfdb1f03f6a2bb0ebc849
hyjin123 <seanhoyeonjin@gmail.com>
2025-02-01 20:59:54 -0500

    exercise 3.3

--- index.js
+++ index.js
@@ -28,16 +28,27 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 });
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons);
-});
-
 app.get('/info', (request, response) => {
   const date = new Date();
   const day = date.toDateString();
   const time = date.toLocaleTimeString();
   response.send(`<p>Phonebook has info for ${persons.length} people </br> ${day} ${time}</p>`);
 });
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
