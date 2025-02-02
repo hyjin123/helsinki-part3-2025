d9238789c430de12f0fa29f4a9251a90fd4beac8
hyjin123 <seanhoyeonjin@gmail.com>
2025-02-01 20:35:56 -0500

    exercise 3.2

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+  const date = new Date();
+  const day = date.toDateString();
+  const time = date.toLocaleTimeString();
+  response.send(`<p>Phonebook has info for ${persons.length} people </br> ${day} ${time}</p>`);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
