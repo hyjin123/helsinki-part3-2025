900033e903fe2f9d9b38a72aaf755417760fb0dc
hyjin123 <seanhoyeonjin@gmail.com>
2025-02-05 18:21:31 -0500

    exercise 3.16

--- index.js
+++ index.js
@@ -52,14 +52,21 @@ app.get('/info', (request, response) => {
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(people => {
     response.json(people);
-  });
+  })
+    .catch(error => next(error));
 });
 
 app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id;
-  Person.findById(id).then(person => {
-    response.json(person);
-  });
+  Person.findById(id)
+    .then(person => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -67,7 +74,8 @@ app.delete('/api/persons/:id', (request, response) => {
   // updated the database list
   Person.findByIdAndDelete(id).then(person => {
     response.json(person);
-  });
+  })
+    .catch(error => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
@@ -92,9 +100,30 @@ app.post('/api/persons', (request, response) => {
 
   newPerson.save().then(savedPerson => {
     response.json(savedPerson);
-  });
+  })
+    .catch(error => next(error));
 
 });
+
+//below are error handler middlewares
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
