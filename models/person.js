965bd2a5ae58f392552cf306a7a12fedb7a8b99a
hyjin123 <seanhoyeonjin@gmail.com>
2025-02-18 20:46:47 -0500

    exercise 3.2

--- models/person.js
+++ models/person.js
@@ -18,7 +18,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /\b\d{2,3}-\d{6,}\b/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number`
+    },
+    minLength: 8,
+    required: true,
+  }
 });
 
 personSchema.set('toJSON', {
