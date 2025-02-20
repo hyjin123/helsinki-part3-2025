d007f6a800cf1cc9d9781d0af2544345a073540a
hyjin123 <seanhoyeonjin@gmail.com>
2025-02-19 20:21:32 -0500

    exercise 3.22

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const morgan = require('morgan');
-const cors = require('cors');
-const Person = require('./models/person');
+const morgan = require("morgan");
+const cors = require("cors");
+const Person = require("./models/person");
 
 const app = express();
 
@@ -11,15 +11,15 @@ morgan.token("body", (req) => {
 });
 
 app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 app.use(cors());
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
 });
 
-app.get('/info', (request, response) => {
+app.get("/info", (request, response, next) => {
   const date = new Date();
   const day = date.toDateString();
   const time = date.toLocaleTimeString();
@@ -32,14 +32,14 @@ app.get('/info', (request, response) => {
 
 });
 
-app.get('/api/persons', (request, response) => {
+app.get("/api/persons", (request, response, next) => {
   Person.find({}).then(people => {
     response.json(people);
   })
     .catch(error => next(error));
 });
 
-app.get('/api/persons/:id', (request, response, next) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
   Person.findById(id)
     .then(person => {
@@ -52,7 +52,7 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error));
 });
 
-app.put('/api/persons/:id', (request, response) => {
+app.put("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
   const body = request.body;
 
@@ -63,7 +63,7 @@ app.put('/api/persons/:id', (request, response) => {
     .catch(error => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
   // updated the database list
   Person.findByIdAndDelete(id).then(person => {
@@ -72,7 +72,7 @@ app.delete('/api/persons/:id', (request, response) => {
     .catch(error => next(error));
 });
 
-app.post('/api/persons', (request, response, next) => {
+app.post("/api/persons", (request, response, next) => {
   const person = request.body;
 
   console.log("this is the request body", person);
@@ -95,7 +95,7 @@ app.post('/api/persons', (request, response, next) => {
 
 //below are error handler middlewares
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
+  response.status(404).send({ error: "unknown endpoint" });
 };
 
 // handler of requests with unknown endpoint
@@ -103,9 +103,9 @@ app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
   }
 
